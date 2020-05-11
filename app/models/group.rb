91fb63dacaa2d3a126cbb848e6243d1cf84ab43c
o-taka-yuma <64815738+o-taka-yuma@users.noreply.github.com>
2020-05-12 08:37:47 +0900

    from

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
-  hasmany :messages
+  has_many :messages
   validates :name, presence: true, uniqueness: true
 
 
